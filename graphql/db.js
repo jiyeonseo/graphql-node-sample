10c907d6205ff5b88a9824f9c5af995bb9f302c7
Jiyeon Seo <jiyeonseo@fb.com>
2018-08-07 12:49:35 +0900

    add mutator

--- graphql/db.js
+++ graphql/db.js
@@ -21,3 +21,15 @@ export const getById = (id) => {
   const filteredPerson = people.filter(person => id === person.id);
   return filteredPerson[0];
 }
+
+export const addPerson = (name, age, gender) => {
+  const newPerson = {
+    id: people.length + 1 ,
+    name,
+    age,
+    gender
+  }
+
+  people.push(newPerson);
+  return newPerson;
+}
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,4 +1,4 @@
-import {people, getById} from "./db";
+import {people, getById, addPerson} from "./db";
 
 const resolvers = {
   Query: {
@@ -6,6 +6,9 @@ const resolvers = {
     people: () => people,
     person: (_, {id}) => getById(id),
   },
+  Mutation: {
+    addPerson: (_, {name, age, gender}) => addPerson(name, age, gender),
+  }
 }
 
 export default resolvers;
